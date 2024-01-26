72ec4d7b0feb198666e416af4196dae5e6b5b71e
ytDragonV6bayku <144830973+ytDragonV6bayku@users.noreply.github.com>
2024-01-26 18:27:22 +0300

    Create 12996550309.lua

---
+++ 12996550309.lua
@@ -0,0 +1,43 @@
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
+local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
+
+local Window = Fluent:CreateWindow({
+    Title = "RunStarSimulator" .. Fluent.Version,
+    SubTitle = "blackcat48efe",
+    TabWidth = 160,
+    Size = UDim2.fromOffset(580, 460),
+    Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
+    Theme = "Dark",
+    MinimizeKey = Enum.KeyCode.P -- Used when theres no MinimizeKeybind
+})
+
+--Fluent provides Lucide Icons https://lucide.dev/icons/ for the tabs, icons are optional
+local Tabs = {
+    Main = Window:AddTab({ Title = "Main", Icon = "" }),
+    Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
+}
+Tabs.Main:AddButton({
+    Title = "Button",
+    Description = "deneme/test123",
+    Callback = function()
+        Window:Dialog({
+            Title = "Title",
+            Content = "This is a dialog",
+            Buttons = {
+                {
+                    Title = "Confirm",
+                    Callback = function()
+                        print("sa")
+                    end
+                },
+                {
+                    Title = "Cancel",
+                    Callback = function()
+                        print("neden?")
+                    end
+                }
+            }
+        })
+    end
+})
