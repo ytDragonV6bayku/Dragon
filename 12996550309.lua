10922dd34de15d5728c8cf99b2f0697224b24da8
ytDragonV6bayku <144830973+ytDragonV6bayku@users.noreply.github.com>
2024-01-26 18:34:03 +0300

    Update 12996550309.lua

--- 12996550309.lua
+++ 12996550309.lua
@@ -1,3 +1,4 @@
+if game .game.placeId == 12996550309 then
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
